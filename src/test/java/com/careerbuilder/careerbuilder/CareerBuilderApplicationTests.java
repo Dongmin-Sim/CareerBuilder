757554d4683193c2d25b4286d52a593f4de0dd1d
codongmin <codongmin@gmail.com>
2024-03-14 13:05:32 +0900

    chore #1 - spring boot 프로젝트 추가
    스프링 프로젝트 추가
    - lombok, spring web, jpa 추가
    
    #   [커밋 타입]  리스트
    #   feat     : 기능 (새로운 기능)
    #   fix        : 버그 (버그 수정)
    #   refactor  : 리팩토링
    #   style    : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test      : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore  : 기타 변경사항 (빌드 스크립트 수정 등)
    #   post    : 블로그 포스트 추가 (신규 포스트 작성 및 수정)

---
+++ src/test/java/com/careerbuilder/careerbuilder/CareerBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerbuilder.careerbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
