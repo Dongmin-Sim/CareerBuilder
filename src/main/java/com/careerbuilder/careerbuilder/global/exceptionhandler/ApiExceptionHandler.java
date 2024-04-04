8e8f3432fae231d5245c855fc84dd5e916166e9c
codongmin <codongmin@gmail.com>
2024-04-04 17:01:50 +0900

    Add #8 - Validation Error & Exception Handler
    유효하지 않은 path variable 예외를 처리하기 위한
    - Validation error 유형 추가
    - Validation 관련 예외를 처리할 수 있는 Exception Handler 추가
    
    - 예상되는 응답 에러 코드를 BadRequest 에서 ValidationError로 변경하므써 예외 발생 이유를 조금 더 명확하게 세분화

--- src/main/java/com/careerbuilder/careerbuilder/global/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/careerbuilder/careerbuilder/global/exceptionhandler/ApiExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.careerbuilder.careerbuilder.global.exceptionhandler;
 
 import com.careerbuilder.careerbuilder.global.common.api.Api;
+import com.careerbuilder.careerbuilder.global.common.error.ErrorCode;
 import com.careerbuilder.careerbuilder.global.common.error.ErrorCodeIfs;
 import com.careerbuilder.careerbuilder.global.common.exception.ApiException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 @Slf4j
 @RestControllerAdvice
@@ -27,5 +30,16 @@ public class ApiExceptionHandler {
                 .status(errorCodeIfs.getHttpStatusCode())
                 .body(Api.ERROR(errorCodeIfs, apiException.getErrorMessage()));
     }
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Api<Object>> validationException(
+            HandlerMethodValidationException handlerMethodValidationException
+    ) {
+        // TODO Exception 메시지 포맷 정리 필요
+        handlerMethodValidationException.getAllValidationResults()
+                .forEach(it-> it.getResolvableErrors().forEach(error-> log.error("[Validation Exception] : {}", error.getDefaultMessage())));
 
+        return ResponseEntity
+                .status(ErrorCode.VALIDATION_ERROR.getHttpStatusCode())
+                .body(Api.ERROR(ErrorCode.VALIDATION_ERROR, ErrorCode.VALIDATION_ERROR.getErrorMessage()));
+    }
 }
