0bc251f78328b1286e97e382f6d2104c9d0a49ed
codongmin <codongmin@gmail.com>
2024-04-11 22:28:32 +0900

    Update #10 - transaction entity
    테이블 변경에 따른 엔티티 및 프로젝트 구조 변경

--- src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/Transaction.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/Transaction.java
@@ -1,9 +1,15 @@
 package com.careerbuilder.careerbuilder.domain.transaction.entity;
 
+import com.careerbuilder.careerbuilder.domain.location.entity.Location;
+import com.careerbuilder.careerbuilder.domain.partner.entity.Partner;
+import com.careerbuilder.careerbuilder.domain.transaction.type.TransactionStatusType;
 import com.careerbuilder.careerbuilder.domain.transaction.type.TransactionType;
 import com.careerbuilder.careerbuilder.global.common.baseentity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
@@ -19,6 +25,22 @@ public class Transaction extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @ManyToOne
+    @JoinColumn(name = "partner_id")
+    private Partner partner;
+
+    private int quantity;
+
+    @Column(length = 200)
+    private String memo;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatusType status;
+
+    @Enumerated(EnumType.STRING)
     private TransactionType transactionType;
 }
---
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/type/TransactionStatusType.java
@@ -0,0 +1,5 @@
+package com.careerbuilder.careerbuilder.domain.transaction.type;
+
+public enum TransactionStatusType {
+    ACTIVE, INACTIVE
+}
