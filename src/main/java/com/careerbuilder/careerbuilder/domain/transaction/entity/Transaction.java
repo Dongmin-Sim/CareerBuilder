cb714fac18552e82a53db4e7be1cc93beda61a9b
codongmin <codongmin@gmail.com>
2024-04-11 23:56:37 +0900

    Update #10 - Transaction Entity
    - 출발지와 도착지를 나타내는 필드 추가
    - 필드 순서 변경

--- src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/Transaction.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/Transaction.java
@@ -2,8 +2,8 @@ package com.careerbuilder.careerbuilder.domain.transaction.entity;
 
 import com.careerbuilder.careerbuilder.domain.location.entity.Location;
 import com.careerbuilder.careerbuilder.domain.partner.entity.Partner;
-import com.careerbuilder.careerbuilder.domain.transaction.type.TransactionStatusType;
-import com.careerbuilder.careerbuilder.domain.transaction.type.TransactionType;
+import com.careerbuilder.careerbuilder.domain.transaction.entity.type.TransactionStatusType;
+import com.careerbuilder.careerbuilder.domain.transaction.entity.type.TransactionType;
 import com.careerbuilder.careerbuilder.global.common.baseentity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,15 +25,20 @@ public class Transaction extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
     @ManyToOne
     @JoinColumn(name = "location_id")
-    private Location location;
+    private Location fromLocation;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location toLocation;
 
     @ManyToOne
     @JoinColumn(name = "partner_id")
     private Partner partner;
-
-    private int quantity;
 
     @Column(length = 200)
     private String memo;
@@ -41,6 +46,4 @@ public class Transaction extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private TransactionStatusType status;
 
-    @Enumerated(EnumType.STRING)
-    private TransactionType transactionType;
 }
