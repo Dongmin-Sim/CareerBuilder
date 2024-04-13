a8f9d0f2ea4f62407768a9987484503284f5b6cf
codongmin <codongmin@gmail.com>
2024-04-13 18:42:14 +0900

    Update #17 - TransactionType name
    거래 종류를 직관성 있게 다음과 같이 변경
    - 입고 : WAREHOUSING -> IN
    - 출고 : DELIVERY -> OUT
    - 조정 : ADJUSTMENT -> ADJUST
    - 이동 : MOVING -> MOVE

--- src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/type/TransactionType.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/type/TransactionType.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum TransactionType {
-    WAREHOUSING("입고"), DELIVERY("출고"), ADJUSTMENT("조정"), MOVING("이동"),
+    IN("입고"), OUT("출고"), ADJUST("조정"), MOVE("이동"),
     ;
 
     private final String name;
