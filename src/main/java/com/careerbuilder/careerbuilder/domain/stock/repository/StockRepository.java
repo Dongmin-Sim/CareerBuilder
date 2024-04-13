9d463ff61fe5bb9be3c550abfa1753a854289bc9
codongmin <codongmin@gmail.com>
2024-04-13 19:29:49 +0900

    implement #17 - increaseStockQuantity logic
    거래 유형(입고, 출고, 조정, 이동)에 따른 상품의 증감 데이터를 받아 재고 수량을 변경하는 로직 추가
    - 입고 로직 추가 : 장소 - 상품의 아이디로 재고를 조회 후 재고 수량을 증가

--- src/main/java/com/careerbuilder/careerbuilder/domain/stock/repository/StockRepository.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/stock/repository/StockRepository.java
@@ -2,9 +2,19 @@ package com.careerbuilder.careerbuilder.domain.stock.repository;
 
 import com.careerbuilder.careerbuilder.domain.stock.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    // select * from stock where locationId = ? and productId;
+    Stock findFirstByLocationIdAndProductIdOrderById(Long locationId, Long productId);
+
+    @Modifying
+    @Query("update Stock s set s.stockQuantity = s.stockQuantity + :quantity where s.locationId = :locationId and s.productId = :productId")
+    int updateStockQuantityByLocationIdAnAndProductId(@Param("locationId") Long locationId, @Param("productId") Long productId, @Param("quantity") int quantity);
 
 }
