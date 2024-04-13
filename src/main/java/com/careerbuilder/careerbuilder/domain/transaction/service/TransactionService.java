02e280b2bbe055f77229ee71424cdc5808d8c07d
codongmin <codongmin@gmail.com>
2024-04-13 19:25:05 +0900

    Add #17 - TransactionService save
    Transaction 등록 로직 추가

--- src/main/java/com/careerbuilder/careerbuilder/domain/transaction/service/TransactionService.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/service/TransactionService.java
@@ -1,6 +1,8 @@
 package com.careerbuilder.careerbuilder.domain.transaction.service;
 
+import com.careerbuilder.careerbuilder.domain.transaction.entity.Transaction;
 import com.careerbuilder.careerbuilder.domain.transaction.repository.TransactionRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ public class TransactionService {
 
     private final TransactionRepository transactionRepository;
 
-
-
+    @Transactional
+    public Transaction save(Transaction transactionEntity) {
+        return transactionRepository.save(transactionEntity);
+    }
 }
