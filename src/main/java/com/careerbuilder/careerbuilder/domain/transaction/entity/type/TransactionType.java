0f00ecb77652316c8abc0083e8a771e9fac320ad
codongmin <codongmin@gmail.com>
2024-05-13 12:17:51 +0900

    Refactor #36 - TransactionType enum 필드명 변경
    name 에서 description 으로 변경 (통일성)

--- src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/type/TransactionType.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/entity/type/TransactionType.java
@@ -1,11 +1,13 @@
 package com.careerbuilder.careerbuilder.domain.transaction.entity.type;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum TransactionType {
     IN("입고"), OUT("출고"), ADJUST("조정"), MOVE("이동"),
     ;
 
-    private final String name;
+    private final String description;
 }
