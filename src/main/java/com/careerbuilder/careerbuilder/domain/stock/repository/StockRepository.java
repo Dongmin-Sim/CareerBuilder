edfc1f06b9c8d3b8515b656eb8ba34c578921834
codongmin <codongmin@gmail.com>
2024-05-13 15:17:44 +0900

    Add #36 - 위치별 상품 재고량 조회, 위치별 재고량 총합 조회 로직
    - 위치 Id 로 모든 상품별 재고량을 조회하는 로직 추가
    - 위치 Id 로 모든 상품 재고량 총합을 조회하는 로직 추가

--- src/main/java/com/careerbuilder/careerbuilder/domain/stock/repository/StockRepository.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/stock/repository/StockRepository.java
@@ -1,5 +1,6 @@
 package com.careerbuilder.careerbuilder.domain.stock.repository;
 
+import com.careerbuilder.careerbuilder.domain.stock.dto.StockQuantityIfs;
 import com.careerbuilder.careerbuilder.domain.stock.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,11 +8,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     // select * from stock where locationId = ? and productId;
     Stock findFirstByLocationIdAndProductIdOrderById(Long locationId, Long productId);
+
+    // select * from stock where locationId = ?;
+    List<Stock> findAllByLocationIdOrderById(Long locationId);
+
+    @Query(
+            value = "select " +
+                    "s.locationId as locationId" +
+                    ", sum(s.stockQuantity) as totalQuantity " +
+                    "from Stock s " +
+                    "group by s.locationId"
+    )
+    List<StockQuantityIfs> findStockTotalQuantityGroupByLocation();
+
 
     @Modifying
     @Query("update Stock s set s.stockQuantity = :quantity where s.locationId = :locationId and s.productId = :productId")
