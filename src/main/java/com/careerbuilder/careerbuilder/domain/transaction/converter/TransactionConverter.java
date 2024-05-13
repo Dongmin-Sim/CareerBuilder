6d10cee61ed45356678cfe255d2d0168e1c11a1e
codongmin <codongmin@gmail.com>
2024-05-13 14:44:28 +0900

    Add #36 - toUpdateEntity, toDetailResponse 변환 메서드 추가
    - TransactionUpdateRequest dto를 받아 Transaction entity로 변경해주는 메서드 추가
    - Transaction entity를 받아 TransactionDetailResponse dto로 변경해주는 메서드 추가

--- src/main/java/com/careerbuilder/careerbuilder/domain/transaction/converter/TransactionConverter.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/transaction/converter/TransactionConverter.java
@@ -1,9 +1,12 @@
 package com.careerbuilder.careerbuilder.domain.transaction.converter;
 
+import com.careerbuilder.careerbuilder.domain.transaction.dto.TransactionDetailResponse;
 import com.careerbuilder.careerbuilder.domain.transaction.dto.TransactionRegisterRequest;
 import com.careerbuilder.careerbuilder.domain.transaction.dto.TransactionResponse;
+import com.careerbuilder.careerbuilder.domain.transaction.dto.TransactionUpdateRequest;
 import com.careerbuilder.careerbuilder.domain.transaction.entity.Transaction;
-import com.careerbuilder.careerbuilder.domain.transaction.entity.type.TransactionStatusType;
+import com.careerbuilder.careerbuilder.domain.transaction.entity.type.TransactionStatus;
+import com.careerbuilder.careerbuilder.domain.transaction.entity.type.TransactionType;
 import com.careerbuilder.careerbuilder.global.common.annotation.Converter;
 import com.careerbuilder.careerbuilder.global.common.error.ErrorCode;
 import com.careerbuilder.careerbuilder.global.common.exception.ApiException;
@@ -13,6 +16,24 @@ import java.util.Optional;
 @Converter
 public class TransactionConverter {
 
+    public Transaction toUpdateEntity(
+            Long originTransactionId,
+            TransactionType transactionType,
+            TransactionUpdateRequest updateRequest
+    ) {
+        return Optional.ofNullable(updateRequest)
+                .map(it -> {
+                    return Transaction.builder()
+                            .originTransactionId(originTransactionId)
+                            .transactionType(transactionType)
+                            .fromLocationId(updateRequest.getFromLocation())
+                            .toLocationId(updateRequest.getToLocation())
+                            .partnerId(updateRequest.getPartner())
+                            .memo(updateRequest.getMemo())
+                            .status(TransactionStatus.ACTIVE)
+                            .build();
+                }).orElseThrow(()->new ApiException(ErrorCode.NULL_POINT_ERROR));
+    }
 
     public Transaction toEntity(
             TransactionRegisterRequest registerRequest
@@ -25,7 +46,7 @@ public class TransactionConverter {
                             .toLocationId(registerRequest.getToLocation())
                             .partnerId(registerRequest.getPartner())
                             .memo(registerRequest.getMemo())
-                            .status(TransactionStatusType.ACTIVE)
+                            .status(TransactionStatus.ACTIVE)
                             .build();
                 }).orElseThrow(()->new ApiException(ErrorCode.NULL_POINT_ERROR));
 
@@ -40,4 +61,19 @@ public class TransactionConverter {
                 .build();
     }
 
+    public TransactionDetailResponse toDetailResponse(
+            Transaction transaction
+    ) {
+        return TransactionDetailResponse.builder()
+                .id(transaction.getId())
+                .transactionType(transaction.getTransactionType())
+                .fromLocation(transaction.getFromLocationId())
+                .toLocation(transaction.getToLocationId())
+                .partner(transaction.getPartnerId())
+                .memo(transaction.getMemo())
+                .status(transaction.getStatus())
+                .createdAt(transaction.getCreatedAt())
+                .updatedAt(transaction.getUpdatedAt())
+                .build();
+    }
 }
