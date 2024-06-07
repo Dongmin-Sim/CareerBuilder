aa362bb13d223f09535dc8142ffa59b157180c16
codongmin <codongmin@gmail.com>
2024-06-07 22:36:40 +0900

    Refactor #42 product entity
    - 불필요한 어노테이션 삭제
    - 제품 수정 로직 수정

--- src/main/java/com/careerbuilder/careerbuilder/domain/product/db/entity/Product.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/product/db/entity/Product.java
@@ -1,23 +1,17 @@
 package com.careerbuilder.careerbuilder.domain.product.db.entity;
 
-import com.careerbuilder.careerbuilder.domain.product.business.dto.PartialUpdateProductRequest;
-import com.careerbuilder.careerbuilder.domain.product.business.dto.UpdateProductRequest;
+import com.careerbuilder.careerbuilder.domain.product.business.dto.ProductRequestDto;
 import com.careerbuilder.careerbuilder.global.common.baseentity.BaseEntity;
-import com.careerbuilder.careerbuilder.global.common.error.ErrorCode;
-import com.careerbuilder.careerbuilder.global.common.exception.ApiException;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -43,31 +37,12 @@ public class Product extends BaseEntity {
     @Column(precision = 12, scale = 4)
     private BigDecimal price;
 
-    public void updateProduct(UpdateProductRequest request) {
-        this.name = request.getName();
-        this.barcode = request.getBarcode();
-        this.photoUrl = request.getPhotoUrl();
-        this.cost = request.getCost();
-        this.price = request.getPrice();
-    }
-
-    public void partialUpdateProduct(PartialUpdateProductRequest request) {
-        Optional.ofNullable(request.getName()).ifPresentOrElse(field->{
-            this.name = field;
-        }, ()-> new ApiException(ErrorCode.NULL_POINT_ERROR));
-        Optional.ofNullable(request.getBarcode()).ifPresentOrElse(field->{
-            this.barcode = field;
-        }, ()-> new ApiException(ErrorCode.NULL_POINT_ERROR));
-
-        Optional.ofNullable(request.getPhotoUrl()).ifPresentOrElse(field->{
-            this.photoUrl = field;
-        }, ()-> new ApiException(ErrorCode.NULL_POINT_ERROR));
-        Optional.ofNullable(request.getCost()).ifPresentOrElse(field->{
-            this.cost = field;
-        }, ()-> new ApiException(ErrorCode.NULL_POINT_ERROR));
-        Optional.ofNullable(request.getPrice()).ifPresentOrElse(field->{
-            this.price = field;
-        }, ()-> new ApiException(ErrorCode.NULL_POINT_ERROR));
+    public void updateProduct(ProductRequestDto.UpdateProductDto request) {
+        this.name = request.name();
+        this.barcode = request.barcode();
+        this.photoUrl = request.photoUrl();
+        this.cost = request.cost();
+        this.price = request.price();
     }
 }
 
