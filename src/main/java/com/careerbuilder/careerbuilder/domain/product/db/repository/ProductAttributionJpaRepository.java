76772b54ab95ebe160375d82bf095afc21988c9d
codongmin <codongmin@gmail.com>
2024-06-10 02:58:55 +0900

    Test #42 - ProductJpaRepository
    - ProductJpaRepository 테스트 코드 작성
    - h2 in-memory db 사용, 테스트 데이터 추가

--- src/main/java/com/careerbuilder/careerbuilder/domain/product/db/repository/ProductAttributionJpaRepository.java
+++ src/main/java/com/careerbuilder/careerbuilder/domain/product/db/repository/ProductAttributionJpaRepository.java
@@ -4,8 +4,6 @@ import com.careerbuilder.careerbuilder.domain.product.db.entity.ProductAttributi
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductAttributionJpaRepository extends JpaRepository<ProductAttribution, Long> {
 }
